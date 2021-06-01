668a23514b6eed1990dc719ca020d6a3b8c196c2
AlexisAlegria <alexisalegriav@gmail.com>
2021-06-01 12:13:03 +0000

    avance hasta ejer81

--- exercises/80-getAverageOfElementsAtProperty/app.js
+++ exercises/80-getAverageOfElementsAtProperty/app.js
@@ -1,1 +1,10 @@
 // Write your function here
+function getAverageOfElementsAtProperty(obj,key){
+    return obj.key.reduce((a,b) => a + b, 0) / key.length;
+}
+
+var obj = {
+    key: [1, 2, 3]
+  };
+  var output = getAverageOfElementsAtProperty(obj, 'key');
+  console.log(output); // --> 2
--- exercises/81-addToFrontOfNew/app.js
+++ exercises/81-addToFrontOfNew/app.js
@@ -1,1 +1,10 @@
 // Write your function here
+function addToFrontOfNew(input, key){
+    let array = [key];
+    return array.concat(input);
+}
+var input = [1, 2];
+var output = addToFrontOfNew(input, 3);
+console.log(output); // --> [3, 1, 2];
+console.log(input); //--> [1, 2]
+
